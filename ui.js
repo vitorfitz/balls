07d0fc0c6630a6d88c45c9a53ac7f01823baafd2
Vitor Souza <vfitz@amazon.com>
2026-04-02 15:35:58 -0300

    pipipupu

--- ui.js
+++ ui.js
@@ -3,7 +3,7 @@
 const d = new Date().getTime();
 console.log(d);
 let battleSeed = d;
-// let battleSeed = 1775097368574;
+// let battleSeed = 1775148714492;
 
 const dramaticCheck = document.getElementById("dramatic-check");
 
