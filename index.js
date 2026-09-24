57aef4eb04ef2c26031d75b216d1b45455e4b64c
Vitor Souza <vfitz@amazon.com>
2026-09-24 16:56:53 -0300

    MORER

--- index.js
+++ index.js
@@ -3166,7 +3166,7 @@ class BallBattle {
         return false;
     }
 
-    async physicsLoop(dt, maxQueueLen = 200) {
+    async physicsLoop(dt, maxQueueLen = 300) {
         while (!this.stopped) {
             if (!this.headless && this.frameQueue.length >= maxQueueLen) {
                 await cheatYield(this);
